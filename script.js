a2a9955e0d86360330385390d8c5f8634c73ec25
Tanya Malik <106032126+MalikTanya@users.noreply.github.com>
2023-06-09 01:30:59 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,81 @@
+const items = document.querySelectorAll(".item");
+const containers = document.querySelectorAll(".item-container");
+const resetBtn = document.getElementById("reset-btn");
+const message = document.getElementById("message");
+
+let draggedItem = null;
+
+// Add event listeners for drag events
+items.forEach((item) => {
+  item.addEventListener("dragstart", dragStart);
+  item.addEventListener("dragend", dragEnd);
+});
+
+// Add event listeners for drop events
+containers.forEach((container) => {
+  container.addEventListener("dragover", dragOver);
+  container.addEventListener("dragenter", dragEnter);
+  container.addEventListener("dragleave", dragLeave);
+  container.addEventListener("drop", drop);
+});
+
+// Reset button event listener
+resetBtn.addEventListener("click", resetContainers);
+
+// Drag Functions
+function dragStart() {
+  draggedItem = this;
+  setTimeout(() => {
+    this.style.opacity = "0.3";
+  }, 0);
+}
+
+function dragEnd() {
+  draggedItem = null;
+  this.style.opacity = "1";
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.style.border = "2px dashed #333";
+}
+
+function dragLeave() {
+  this.style.border = "2px dashed #aaa";
+}
+
+function drop() {
+  this.style.border = "2px dashed #aaa";
+  this.appendChild(draggedItem);
+  showMessage("Item dropped successfully! 🎉", "success");
+}
+
+// Reset Containers
+function resetContainers() {
+  containers.forEach((container) => {
+    container.innerHTML = "";
+  });
+
+  items.forEach((item) => {
+    document.getElementById("container1").appendChild(item);
+  });
+
+  showMessage("Containers reset! 🔄", "info");
+}
+
+// Show Message
+function showMessage(text, type) {
+  message.textContent = text;
+  message.classList.add(type);
+  message.classList.add("show");
+
+  setTimeout(() => {
+    message.classList.remove("show");
+    message.textContent = "";
+    message.classList.remove(type);
+  }, 2000);
+}
